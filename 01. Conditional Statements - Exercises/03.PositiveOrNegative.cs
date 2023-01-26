dfed90042f0c3603ec0d00189275c885d82b9256
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 22:07:26 +0200

    Add files via upload

---
+++ 01. Conditional Statements - Exercises/03.PositiveOrNegative.cs
@@ -0,0 +1,19 @@
+﻿//Write a C# Sharp program to check whether
+//a given number is positive or negative.
+
+//INPUT
+double number = double.Parse(Console.ReadLine());
+
+//ACTION
+if (number > 0)
+{
+    Console.WriteLine($"{number} is a positive number.");
+}
+else if (number < 0)
+{
+    Console.WriteLine($"{number} is a negative number.");
+}
+else
+{
+    Console.WriteLine("Your given number is 0.");
+}
---
+++ 01. Conditional Statements - Exercises/04.LeapYear.cs
@@ -0,0 +1,15 @@
+﻿//Write a C# Sharp program to find whether
+//a given year is a leap year or not.
+
+//INPUT
+int year = int.Parse(Console.ReadLine());
+
+//ACTION
+if (year % 4 == 0)
+{
+    Console.WriteLine($"{year} is a leap year.");
+}
+else
+{
+    Console.WriteLine($"{year} is not a leap year.");
+}
