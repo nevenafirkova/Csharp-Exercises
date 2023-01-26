079f78c64f3d3f667e3100ed5845a5dcafedfe79
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 22:07:52 +0200

    Add files via upload

---
+++ 01. Conditional Statements - Exercises/05.EligibleVote.cs
@@ -0,0 +1,20 @@
+﻿//Write a C# Sharp program to read the age
+//of a candidate and determine whether it is
+//eligible for casting his/her own vote.
+
+//INPUT
+int age = int.Parse(Console.ReadLine());
+
+//ACTION
+if (age >= 21)
+{
+    Console.WriteLine("Congratulations! You are eligible for casting your vote.");
+}
+else if (21 - age == 1)
+{
+    Console.WriteLine($"Sorry, you're still not eligible for casting your vote. You need {21 - age} more year to vote.");
+}
+else
+{
+    Console.WriteLine($"Sorry, you're still not eligible for casting your vote. You need {21 - age} more years to vote.");
+}
---
+++ 01. Conditional Statements - Exercises/06.DisplayValue.cs
@@ -0,0 +1,10 @@
+﻿//Write a C# Sharp program to read the value
+//of an integer m and display the value of n
+//is 1 when m is larger than 0, 0 when m is
+//0 and -1 when m is less than 0.
+
+//INPUT
+int number = int.Parse(Console.ReadLine());
+
+//ACTION
+
