652ac6a88ad08fae5065218b817e7b3c0036ff9c
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 22:07:12 +0200

    Add files via upload

---
+++ 01. Conditional Statements - Exercises/01.EqualIntegers.cs
@@ -0,0 +1,19 @@
+﻿//Write a C# Sharp program to accept two integers
+//and check whether they are equal or not.
+
+//INPUT
+int[] numbers = new int[2];
+numbers[0] = int.Parse(Console.ReadLine());
+numbers[1] = int.Parse(Console.ReadLine());
+
+//ACTION
+if (numbers[0] == numbers[1])
+{
+    //Output
+    Console.WriteLine($"Numbers {numbers[0]} and {numbers[1]} are equal.");
+}
+else
+{
+    //Output
+    Console.WriteLine($"Numbers {numbers[0]} and {numbers[1]} are not equal.");
+}
---
+++ 01. Conditional Statements - Exercises/02.EvenOrOdd.cs
@@ -0,0 +1,15 @@
+﻿//Write a C# Sharp program to check whether
+//a given number is even or odd.
+
+//INPUT
+int number = int.Parse(Console.ReadLine());
+
+//ACTION
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} is an even number.");
+}
+else
+{
+    Console.WriteLine($"{number} is an odd number.");
+}
