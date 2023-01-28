330f7e5b7a1cafb98ce09bce337af5c4b29af3da
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-28 21:06:48 +0200

    Add files via upload

---
+++ 03. Arrays - Exercises/02.ReverseArrayPrint.cs
@@ -0,0 +1,27 @@
+﻿//Write a program in C# Sharp to read n number
+//of values in an array and display it in reverse order.
+
+//INPUT
+int[] numbers = new int[int.Parse(Console.ReadLine())];
+
+//ACTION
+for (
+    int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine()); 
+}
+
+//OUTPUT
+
+Console.Write("The values store into the array are: ");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
+
+Console.WriteLine();
+Console.Write("The values store into the array in reverse are: ");
+for (int j = numbers.Length -1; j >= 0; j--)
+{
+    Console.Write($"{numbers[j]} ");
+}
