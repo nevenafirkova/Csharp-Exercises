d00dc1a55c78133788a0ddb640d9665173568661
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-28 21:03:36 +0200

    Add files via upload

---
+++ 03. Arrays - Exercises/01.PrintArray.cs
@@ -0,0 +1,13 @@
+﻿//Write a program in C# Sharp to store
+//elements in an array and print it.
+
+//INPUT
+int[] ints = new int[int.Parse(Console.ReadLine())];
+
+//OUTPUT
+Console.Write("Elements in the array are: ");
+
+for (int i = 0; i < ints.Length; i++)
+{
+    Console.Write($"{i} ");
+}
