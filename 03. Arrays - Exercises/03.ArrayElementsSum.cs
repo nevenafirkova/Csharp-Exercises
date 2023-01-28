45351d354f41cf215912652d2e508314b9604340
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-28 21:08:28 +0200

    Add files via upload

---
+++ 03. Arrays - Exercises/03.ArrayElementsSum.cs
@@ -0,0 +1,22 @@
+﻿//Write a program in C# Sharp to find the
+//sum of all elements of the array.
+
+//INPUT
+int[] numbers = new int[int.Parse(Console.ReadLine())];
+int numbersSum = 0;
+
+//ACTION
+for (int i = 0; i < numbers.Length; i++)
+{
+    //Elements Values
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+//Sum
+for (int j = 0; j < numbers.Length; j++)
+{
+    numbersSum += numbers[j];
+}
+
+//OUTPUT
+Console.WriteLine($"Sum of all elements stored in the array is: {numbersSum}");
