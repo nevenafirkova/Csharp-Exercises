981349fbdfb1a31c5dac21d4b50851432555c892
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-28 21:09:04 +0200

    Add files via upload

---
+++ 03. Arrays - Exercises/05.NumberDublicatedElements.cs
@@ -0,0 +1,19 @@
+﻿//Write a program in C# Sharp to count a total
+//number of duplicate elements in an array.
+
+//INPUT
+int[] arrayOne = new int[int.Parse(Console.ReadLine())];
+int[] arrayTwo = new int[arrayOne.Length];
+
+//ACTION
+for (int i = 0; i < arrayOne.Length; i++)
+{
+    arrayOne[i] = int.Parse(Console.ReadLine());
+}
+
+for (int j = 0; j < arrayOne.Length; j++)
+{
+    arrayTwo[j] = arrayOne[j];
+}
+
+
