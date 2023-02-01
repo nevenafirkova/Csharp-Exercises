1b5808405362caaf9524a5464098ed3f017204cb
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-01 16:16:10 +0200

    Add files via upload

---
+++ 02. For Loop - Exercises/04.SumAndAverage.cs
@@ -0,0 +1,15 @@
+﻿//Write a program in C# Sharp to read 10 numbers from keyboard and find their sum and average.
+
+//ACTION
+double numberInput;
+double numbersSum = 0;
+
+for (int i = 1; i < 10; i++)
+{
+    numberInput = double.Parse(Console.ReadLine());
+    numbersSum += numberInput;
+}
+
+//OUTPUT
+Console.WriteLine($"The sum of the ten numbers is: {numbersSum}");
+Console.WriteLine($"The average is: {numbersSum / 10.0}");
