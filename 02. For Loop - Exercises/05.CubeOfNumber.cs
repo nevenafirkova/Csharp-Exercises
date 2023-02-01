ff1630640d7097dbf7aaf92df8bc339db361ac6b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-01 16:16:23 +0200

    Add files via upload

---
+++ 02. For Loop - Exercises/05.CubeOfNumber.cs
@@ -0,0 +1,13 @@
+﻿//Write a program in C# Sharp to display the cube of the number up to given an integer.
+
+//INPUT
+int numbersCnt = int.Parse(Console.ReadLine());
+
+//ACTION
+for (int i = 1; i <= numbersCnt; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+
+    //OUTPUT
+    Console.WriteLine($"The current number is: {currentNumber} and cube of it is: {currentNumber * currentNumber * currentNumber}");
+}
