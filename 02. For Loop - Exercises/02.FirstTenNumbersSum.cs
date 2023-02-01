e73645e3fed19160a8372e4d1a800c37a7c4c451
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-01 16:15:45 +0200

    Add files via upload

---
+++ 02. For Loop - Exercises/02.FirstTenNumbersSum.cs
@@ -0,0 +1,12 @@
+﻿//Write a C# Sharp program to find the sum of first 10 natural numbers.
+
+//ACTION
+int numbersSum = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    numbersSum += i;
+}
+
+//OUTPUT
+Console.WriteLine($"The sum is: {numbersSum}");
