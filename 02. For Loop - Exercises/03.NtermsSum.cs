5cbb21e05b20b7f5a8df162eea37bd5f719545cb
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-01 16:15:55 +0200

    Add files via upload

---
+++ 02. For Loop - Exercises/03.NtermsSum.cs
@@ -0,0 +1,16 @@
+﻿//Write a program in C# Sharp to display n terms of natural number and their sum.
+
+//INPUT
+int inputNumber = int.Parse(Console.ReadLine());
+
+//CHANGEABLE
+int numbersSum = 0;
+
+//ACTION
+for (int i = 1; i <= inputNumber; i++)
+{
+    numbersSum += i;
+}
+
+//OUTPUT
+Console.WriteLine($"The sum of all natural numbers up to {inputNumber} terms is: {numbersSum}.");
